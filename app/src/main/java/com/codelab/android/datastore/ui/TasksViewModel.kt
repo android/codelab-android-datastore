007cf2f0a550420a6e2722a7b64d9acdb850e568
rohitsat13 <rohitsat@google.com>
2020-08-24 14:24:18 -0700

    Make changes to ProtoDataStore: 1) use context.createDataStore factory 2) Fix bug where unspecified value is not handled 3) Update SharedPreferencesMigration to use the updated Constructor

--- app/src/main/java/com/codelab/android/datastore/ui/TasksViewModel.kt
+++ app/src/main/java/com/codelab/android/datastore/ui/TasksViewModel.kt
@@ -72,6 +72,7 @@ class TasksViewModel(
         }
         // sort the tasks
         return when (sortOrder) {
+            SortOrder.UNSPECIFIED -> filteredTasks // Treat unspecified order as NONE
             SortOrder.NONE -> filteredTasks
             SortOrder.BY_DEADLINE -> filteredTasks.sortedByDescending { it.deadline }
             SortOrder.BY_PRIORITY -> filteredTasks.sortedBy { it.priority }
