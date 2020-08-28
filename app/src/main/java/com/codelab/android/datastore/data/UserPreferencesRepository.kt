f13e526bd14cac6159b1563ea46d15c056523285
rohitsat13 <rohitsat@google.com>
2020-08-28 09:10:30 -0700

    Remove wild card imports from UserPreferencesRepository

--- app/src/main/java/com/codelab/android/datastore/data/UserPreferencesRepository.kt
+++ app/src/main/java/com/codelab/android/datastore/data/UserPreferencesRepository.kt
@@ -19,7 +19,12 @@ package com.codelab.android.datastore.data
 import android.content.Context
 import android.util.Log
 import androidx.datastore.DataStore
-import androidx.datastore.preferences.*
+import androidx.datastore.preferences.Preferences
+import androidx.datastore.preferences.SharedPreferencesMigration
+import androidx.datastore.preferences.createDataStore
+import androidx.datastore.preferences.edit
+import androidx.datastore.preferences.emptyPreferences
+import androidx.datastore.preferences.preferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
