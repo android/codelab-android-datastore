336760a0f382bbaec471c25359f28ce644f36a34
rohitsat13 <rohitsat@google.com>
2020-11-12 19:00:10 -0800

    Modify imports and version number

--- app/src/main/java/com/codelab/android/datastore/data/UserPreferencesRepository.kt
+++ app/src/main/java/com/codelab/android/datastore/data/UserPreferencesRepository.kt
@@ -18,13 +18,13 @@ package com.codelab.android.datastore.data
 
 import android.content.Context
 import android.util.Log
-import androidx.datastore.DataStore
-import androidx.datastore.preferences.Preferences
+import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.SharedPreferencesMigration
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.preferencesKey
 import androidx.datastore.preferences.createDataStore
-import androidx.datastore.preferences.edit
-import androidx.datastore.preferences.emptyPreferences
-import androidx.datastore.preferences.preferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
